0dd49ee5d8790d6fc52a16651f99094a6220ca62
DFKorn <d1man2192@mail.ru>
2025-10-02 21:49:46 +0300

    add increment\decrement

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,14 @@ function App() {
 }
 
 const OtherComponent = ({ count }: { count: number }) => {
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
   return (
     <div>
       count is {count}
       <div>
-        <button>Increment</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
       </div>
     </div>
   );
