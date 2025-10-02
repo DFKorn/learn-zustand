a6bfb91930596de3d5d23d9c54056b3894a8663e
DFKorn <d1man2192@mail.ru>
2025-10-02 22:16:03 +0300

    add async func

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,13 @@ function App() {
 const OtherComponent = ({ count }: { count: number }) => {
   const increment = useCounterStore((state) => state.increment);
   const decrement = useCounterStore((state) => state.decrement);
+  const incrementAsync = useCounterStore((state) => state.incrementAsync);
   return (
     <div>
       count is {count}
       <div>
         <button onClick={increment}>Increment</button>
+        <button onClick={incrementAsync}>Increment Async</button>
         <button onClick={decrement}>Decrement</button>
       </div>
     </div>
